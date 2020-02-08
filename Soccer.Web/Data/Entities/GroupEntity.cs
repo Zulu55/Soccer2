297502cec9afac09e4d8f4fd3b821a753422432e
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-02-08 13:30:28 -0500

    Add tournament entity

---
+++ Soccer.Web/Data/Entities/GroupEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class GroupEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        public TournamentEntity Tournament { get; set; }
+    }
+}
---
+++ Soccer.Web/Data/Entities/TournamentEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class TournamentEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; }
+
+        public ICollection<GroupEntity> Groups { get; set; }
+    }
+}
