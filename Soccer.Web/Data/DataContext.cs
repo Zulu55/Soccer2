3bf48f609379baf9fd0d6073a6bdd6a97da66397
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-02-12 18:46:14 -0500

    Complete tournament entities

--- Soccer.Web/Data/DataContext.cs
+++ Soccer.Web/Data/DataContext.cs
@@ -9,6 +9,15 @@ namespace Soccer.Web.Data
         {
         }
 
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
+
         public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
     }
 }
---
+++ Soccer.Web/Data/Entities/MatchEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class MatchEntity
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime DateLocal => Date.ToLocalTime();
+
+        public TeamEntity Local { get; set; }
+
+        public TeamEntity Visitor { get; set; }
+
+        [Display(Name = "Goals Local")]
+        public int GoalsLocal { get; set; }
+
+        [Display(Name = "Goals Visitor")]
+        public int GoalsVisitor { get; set; }
+
+        [Display(Name = "Is Closed?")]
+        public bool IsClosed { get; set; }
+
+        public GroupEntity Group { get; set; }
+    }
+}
