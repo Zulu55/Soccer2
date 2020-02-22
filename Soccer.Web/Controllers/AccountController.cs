a78e9cc61280e12c868bf829ae02dff59fc59a7c
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-02-21 20:57:53 -0500

    Login & Logout

---
+++ Soccer.Web/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Soccer.Web.Helpers;
+using Soccer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserHelper _userHelper;
+
+        public AccountController(IUserHelper userHelper)
+        {
+            _userHelper = userHelper;
+        }
+
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userHelper.LoginAsync(model);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Email or password incorrect.");
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Soccer.Web/Controllers/TournamentsController.cs
+++ Soccer.Web/Controllers/TournamentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Soccer.Web.Data;
 using Soccer.Web.Data.Entities;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace Soccer.Web.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class TournamentsController : Controller
     {
         private readonly DataContext _context;
