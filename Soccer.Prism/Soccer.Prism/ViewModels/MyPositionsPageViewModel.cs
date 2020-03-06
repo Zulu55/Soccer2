2f9c09f8cc265a1905eba0481aaf230128be6e2e
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-03-06 06:37:41 -0500

    Show my positions

--- Soccer.Prism/Soccer.Prism/ViewModels/MyPositionsPageViewModel.cs
+++ Soccer.Prism/Soccer.Prism/ViewModels/MyPositionsPageViewModel.cs
@@ -1,12 +1,64 @@
-﻿using Prism.Navigation;
+﻿using Newtonsoft.Json;
+using Prism.Navigation;
+using Soccer.Common.Helpers;
+using Soccer.Common.Models;
+using Soccer.Common.Services;
+using Soccer.Prism.Helpers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Soccer.Prism.ViewModels
 {
     public class MyPositionsPageViewModel : ViewModelBase
     {
-        public MyPositionsPageViewModel(INavigationService navigationService) : base(navigationService)
+        private readonly IApiService _apiService;
+        private bool _isRunning;
+        private ObservableCollection<PositionResponse> _positions;
+
+        public MyPositionsPageViewModel(INavigationService navigationService, IApiService apiService)
+            : base(navigationService)
         {
-            Title = "My Positions";
+            _apiService = apiService;
+            Title = Languages.Positions;
+            LoadPositionsAsync();
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public ObservableCollection<PositionResponse> Positions
+        {
+            get => _positions;
+            set => SetProperty(ref _positions, value);
+        }
+
+        private async void LoadPositionsAsync()
+        {
+            IsRunning = true;
+            string url = App.Current.Resources["UrlAPI"].ToString();
+            bool connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.ConnectionError, Languages.Accept);
+                return;
+            }
+
+            TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(Settings.Token);
+            Response response = await _apiService.GetListAsync<PositionResponse>(url, "/api", $"/Predictions", "bearer", token.Token);
+            IsRunning = false;
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
+                return;
+            }
+
+            List<PositionResponse> list = (List<PositionResponse>)response.Result;
+            Positions = new ObservableCollection<PositionResponse>(list);
         }
     }
 }
