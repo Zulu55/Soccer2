21674a6fcdcf17a58bc4772033d6affec853a0f1
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-03-06 19:19:42 -0500

    Add icon & splash

--- Soccer.Prism/Soccer.Prism.Android/MainActivity.cs
+++ Soccer.Prism/Soccer.Prism.Android/MainActivity.cs
@@ -7,7 +7,12 @@ using Syncfusion.SfBusyIndicator.XForms.Droid;
 
 namespace Soccer.Prism.Droid
 {
-    [Activity(Label = "Soccer.Prism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Soccer", 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
