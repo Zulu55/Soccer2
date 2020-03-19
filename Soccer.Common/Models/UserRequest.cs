03a3e3e43fc907ced83654adfadb52d5697b69e6
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-03-18 20:00:32 -0500

    API for register new users

---
+++ Soccer.Common/Models/UserRequest.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Common.Models
+{
+    public class UserRequest
+    {
+        [Required]
+        public string Document { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public int TeamId { get; set; }
+
+        public byte[] PictureArray { get; set; }
+
+        [Required]
+        public string CultureInfo { get; set; }
+    }
+}
---
+++ Soccer.Web/Controllers/API/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Soccer.Common.Enums;
+using Soccer.Common.Models;
+using Soccer.Web.Data;
+using Soccer.Web.Data.Entities;
+using Soccer.Web.Helpers;
+using System.Globalization;
+using Soccer.Web.Resources;
+
+namespace Soccer.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        private readonly IUserHelper _userHelper;
+        private readonly IMailHelper _mailHelper;
+        private readonly IImageHelper _imageHelper;
+
+        public AccountController(
+            DataContext dataContext,
+            IUserHelper userHelper,
+            IMailHelper mailHelper,
+            IImageHelper imageHelper)
+        {
+            _dataContext = dataContext;
+            _userHelper = userHelper;
+            _mailHelper = mailHelper;
+            _imageHelper = imageHelper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] UserRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Bad request",
+                    Result = ModelState
+                });
+            }
+
+            CultureInfo cultureInfo = new CultureInfo(request.CultureInfo);
+            Resource.Culture = cultureInfo;
+
+            UserEntity user = await _userHelper.GetUserAsync(request.Email);
+            if (user != null)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = Resource.UserAlreadyExists
+                });
+            }
+
+            string picturePath = string.Empty;
+            if (request.PictureArray != null && request.PictureArray.Length > 0)
+            {
+                picturePath = _imageHelper.UploadImage(request.PictureArray, "Users");
+            }
+
+            user = new UserEntity
+            {
+                Address = request.Address,
+                Document = request.Document,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                PhoneNumber = request.Phone,
+                UserName = request.Email,
+                PicturePath = picturePath,
+                UserType = UserType.User,
+                Team = await _dataContext.Teams.FindAsync(request.TeamId)
+            };
+
+            IdentityResult result = await _userHelper.AddUserAsync(user, request.Password);
+            if (result != IdentityResult.Success)
+            {
+                return BadRequest(result.Errors.FirstOrDefault().Description);
+            }
+
+            UserEntity userNew = await _userHelper.GetUserAsync(request.Email);
+            await _userHelper.AddUserToRoleAsync(userNew, user.UserType.ToString());
+
+            string myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+            string tokenLink = Url.Action("ConfirmEmail", "Account", new
+            {
+                userid = user.Id,
+                token = myToken
+            }, protocol: HttpContext.Request.Scheme);
+
+            _mailHelper.SendMail(request.Email, Resource.ConfirmEmail, $"<h1>{Resource.ConfirmEmail}</h1>" +
+                $"{Resource.ConfirmEmailSubject}</br></br><a href = \"{tokenLink}\">{Resource.ConfirmEmail}</a>");
+
+            return Ok(new Response
+            {
+                IsSuccess = true,
+                Message = Resource.ConfirmEmailMessage
+            });
+        }
+    }
+}
