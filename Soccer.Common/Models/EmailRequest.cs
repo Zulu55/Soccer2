450cb9a2f81d0792fe3e8443cc7c0b98fc70189f
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-03-18 20:13:50 -0500

    API to recover password

---
+++ Soccer.Common/Models/EmailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Common.Models
+{
+    public class EmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string CultureInfo { get; set; }
+    }
+}
--- Soccer.Web/Controllers/API/AccountController.cs
+++ Soccer.Web/Controllers/API/AccountController.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
-using System.Threading.Tasks;
 using Soccer.Common.Enums;
 using Soccer.Common.Models;
 using Soccer.Web.Data;
 using Soccer.Web.Data.Entities;
 using Soccer.Web.Helpers;
-using System.Globalization;
 using Soccer.Web.Resources;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Soccer.Web.Controllers.API
 {
@@ -30,6 +30,45 @@ namespace Soccer.Web.Controllers.API
             _userHelper = userHelper;
             _mailHelper = mailHelper;
             _imageHelper = imageHelper;
+        }
+
+        [HttpPost]
+        [Route("RecoverPassword")]
+        public async Task<IActionResult> RecoverPassword([FromBody] EmailRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Bad request"
+                });
+            }
+
+            CultureInfo cultureInfo = new CultureInfo(request.CultureInfo);
+            Resource.Culture = cultureInfo;
+
+            UserEntity user = await _userHelper.GetUserAsync(request.Email);
+            if (user == null)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = Resource.UserDoesntExists
+                });
+            }
+
+            string myToken = await _userHelper.GeneratePasswordResetTokenAsync(user);
+            string link = Url.Action("ResetPassword", "Account", new { token = myToken }, protocol: HttpContext.Request.Scheme);
+            _mailHelper.SendMail(request.Email, Resource.RecoverPassword, $"<h1>{Resource.RecoverPassword}</h1>" +
+                $"{Resource.RecoverPasswordSubject}:</br></br>" +
+                $"<a href = \"{link}\">{Resource.RecoverPassword}</a>");
+
+            return Ok(new Response
+            {
+                IsSuccess = true,
+                Message = Resource.RecoverPasswordMessage
+            });
         }
 
         [HttpPost]
