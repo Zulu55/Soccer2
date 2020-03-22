da1edcbbe695c9aad85b9a919a7915b56eca84d4
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-03-22 17:07:56 -0500

    Fix recover and change password on MVC controller

--- Soccer.Web/Controllers/AccountController.cs
+++ Soccer.Web/Controllers/AccountController.cs
@@ -41,13 +41,13 @@ namespace Soccer.Web.Controllers
             _mailHelper = mailHelper;
         }
 
-        public IActionResult RecoverPassword()
+        public IActionResult RecoverPasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        public async Task<IActionResult> RecoverPasswordMVC(RecoverPasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -139,13 +139,13 @@ namespace Soccer.Web.Controllers
             return BadRequest();
         }
 
-        public IActionResult ChangePassword()
+        public IActionResult ChangePasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        public async Task<IActionResult> ChangePasswordMVC(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
